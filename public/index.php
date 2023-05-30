e200fb2938bde6950ed26c8175eec25e99809a6b
Ramon Parra <ramonpv@gmail.com>
2023-05-30 22:38:30 +0200

    Primera subida a produccion del back

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Kernel;
+// Esto Hace que funcione CORS y se puedan hacer peticiones desde la pagina al servidor
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
